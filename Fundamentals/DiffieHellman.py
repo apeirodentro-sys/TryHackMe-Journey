6c8fd806912ca83c6aac677bd6c2d3e232e226fc
apeirodentro-sys <apeirodentro@gmail.com>
2026-09-13 12:31:11 -0300

    feat: Implemented RSA sign verifier

--- Fundamentals/DiffieHellman.py
+++ Fundamentals/DiffieHellman.py
@@ -1,4 +1,56 @@
+from cryptography.exceptions import InvalidSignature
+from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+import cryptography.exceptions
+
+class client:
+    def __init__(self, rsa_public_key: rsa.RSAPublicKey):
+        self.rsa_public_key = rsa_public_key
+    def verify(self, signature: bytes, message: bytes):
+        try:
+            self.rsa_public_key.verify(
+                signature,
+                message,
+                padding.PSS(
+                    mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.DIGEST_LENGTH,
+                ),
+                algorithm=hashes.SHA256(),
+            )
+            print("Valid signature: the message and signer are authentic.")
+        except InvalidSignature:
+            print("Invalid Signature")
+            
+
+class server:
+    def __init__(self):
+        self.rsa_private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+        self.rsa_public_key = self.rsa_private_key.public_key()
+        self.message = b"I am who I am"
+    def sign(self):
+        signature = self.rsa_private_key.sign(
+            self.message,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.DIGEST_LENGTH,
+            ),
+            algorithm=hashes.SHA256(),
+            )
+        return signature
+
+
+server1 = server()
+signature = server1.sign()
+
+
+client1 = client(server1.rsa_public_key)
+client1.verify(signature, server1.message)
+
+
+
+
 
 parameters = dh.generate_parameters(2, 2048)
 server_private_key = parameters.generate_private_key()
